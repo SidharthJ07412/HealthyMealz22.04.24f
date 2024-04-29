3bdbfcf8326a6ee1a5cd35008b8918837e4f4a87
Sidharth <sidharth07412@gmail.com>
2024-04-30 00:21:45 +0530

    routes update

--- routes/Delivery_route.js
+++ routes/Delivery_route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {fetch_mess_id,fetch_mess_users,delete_request,accept_request,fetch_hire_requests,fetch_current_mess,fetch_mess_loc} from "../controllers/Delivery_controller.js"
+import {fetch_mess_id,fetch_mess_users,delete_request,accept_request,fetch_hire_requests,fetch_current_mess,fetch_mess_loc, set_del_status, fetch_del_status} from "../controllers/Delivery_controller.js"
 const router = express.Router();
 
 router.post("/fetch_current_mess", fetch_current_mess);
@@ -9,6 +9,10 @@ router.post("/accept_request", accept_request);
 router.post("/fetch_mess_id", fetch_mess_id);
 router.post("/fetch_mess_users", fetch_mess_users);
 router.post("/fetch_mess_loc", fetch_mess_loc);
+router.post("/set_del_status",set_del_status);
+router.post("/fetch_del_status",fetch_del_status);
+
+
 // router.post("/DeliveryMapPage")
 
 export default router
